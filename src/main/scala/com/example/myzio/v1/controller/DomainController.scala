2c5f9edcc5b34435b322dfa40c0182fbdf132abe
Pedro García-Valenciano <gvl.pedro2@gmail.com>
2023-11-25 19:37:38 +0100

    mend

--- src/main/scala/com/example/myzio/v1/controller/DomainController.scala
+++ src/main/scala/com/example/myzio/v1/controller/DomainController.scala
@@ -1,20 +1,8 @@
 package com.example.myzio.v1.controller
 
-import com.example.myzio.v1.service.DomainService
-import com.example.myzio.v1.model.domain.Domain
-
 import zio.*
 import zio.http.*
-import zio.http.ChannelEvent.Read
-import zio.http.Header.{AccessControlAllowMethods, AccessControlAllowOrigin, Origin}
 import zio.http.codec.PathCodec.string
-
-import zio.logging.fileLogger
-import zio.logging.LogAnnotation
-import zio.config.typesafe.TypesafeConfigProvider
-import zio.{Config, ConfigProvider, ExitCode, Runtime, Scope, ZIO, ZIOAppDefault, ZLayer}
-
-import zio.json.*
 
 object DomainController {
   def apply(): HttpApp[Any] = {
@@ -23,6 +11,7 @@ object DomainController {
         handler { (id: String, req: Request) =>
           for {
             _ <- ZIO.logInfo("URL:" + req.url.path.toString)
+            // _ <- req.body.asString.map(Response.text(_))
             out <- ZIO.succeed(Response.text(id))
           } yield out
         }
